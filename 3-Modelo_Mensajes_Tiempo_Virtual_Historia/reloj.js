ea13c14a2e2e0c7ed7e1748dfafdc1e90a026b52
mcerdeno2021 <m.cerdeno.2021@alumnos.urjc.es>
2025-10-20 19:52:09 +0200

    Se empiezan a manejar los eventos pausar, retroceder, desde historia con tiempoActual y mandando los eventos desde el reloj

--- 3-Modelo_Mensajes_Tiempo_Virtual_Historia/reloj.js
+++ 3-Modelo_Mensajes_Tiempo_Virtual_Historia/reloj.js
@@ -2,5 +2,10 @@ AFRAME.registerComponent('reloj', {
   tick: function (time, delta) {
     time = time / 1000;
     this.el.emit('reloj-tick', {time: time, delta:delta});
+    if (time > 7) {
+      this.el.emit('control-historia', {accion: "retroceder"});
+    } else {
+      this.el.emit('control-historia', {accion: "reanudar"});
+    }
   }
 });
