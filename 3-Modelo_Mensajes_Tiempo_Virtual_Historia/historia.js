b9f0dd088bf24c9bedddeb771fcd7af161dca990
mcerdeno2021 <m.cerdeno.2021@alumnos.urjc.es>
2025-10-20 19:52:09 +0200

    Avance Apartado 3

--- 3-Modelo_Mensajes_Tiempo_Virtual_Historia/historia.js
+++ 3-Modelo_Mensajes_Tiempo_Virtual_Historia/historia.js
@@ -9,19 +9,21 @@ AFRAME.registerComponent('historia', {
       this.posicionesY = {};
       this.posicionesZ = {};
       this.keysUsadas = [];
+      this.keysEventos = [];
 
       this.pausa = false;
-      this.direccion = "forward"
+      this.direccion = "forward";
+      this.arranque = false;
 
       el.addEventListener('historico', e => {
         this.historia = {
           posicionOrigen: e.detail.origen,
           posicionDestino: e.detail.destino,
           tiempoOrigen: e.detail.tiempoOrigen,
-          tiempoDestino: e.detail.tiempoDestino
+          tiempoDestino: e.detail.tiempoDestino,
         };
 
-        const movimientos = this.historia.tiempoDestino - this.historia.tiempoOrigen; // cuantos moviemiento? tiene sentido el numero por tiempo
+        const movimientos = this.historia.tiempoDestino - this.historia.tiempoOrigen; // cuantos moviemiento? un movimiento en cada tick de reloj?
 
         for (let i = 0; i <= movimientos; i++) {
           let ciclo = this.historia.tiempoOrigen+i;
@@ -32,27 +34,68 @@ AFRAME.registerComponent('historia', {
           this.posicionesY[ciclo] = y;
           this.posicionesZ[ciclo] = z
         }
+
+        this.currentKey = this.historia.tiempoOrigen;
       })
       
+      el.addEventListener('control-historia', e => {
+        if (e.detail.accion === "pausar" ) {
+          this.pausa = true;
+          this.arranque = true;
+        } else if (e.detail.accion === "reanudar") {
+          this.pausa = false;
+          this.arranque = true;
+        } else if (e.detail.accion === "retroceder") {
+          this.pausa = false;
+          this.direccion = "backward";
+          this.arranque = true;
+        } else if (e.detail.accion === "avanzar") {
+          this.pausa = false;
+          this.direccion = "forward";
+          this.arranque = true;
+        }
+      });
+
       el.addEventListener('reloj-tick', e => {
         if (this.pausa) return; //Detiene
 
         const time = e.detail.time;
         const delta = e.detail.delta;
         
-        for (const key in this.posicionesX) { //con hacerlo en X vale para las tres
-          const keyNum = Number(key);
-          
-          if (!(keyNum in this.keysUsadas) && ((keyNum - time) <= delta/1000)) { //dividido porque hablamos de milisegundos  
-            el.emit('mensaje', {
-              posicionesX: this.posicionesX[keyNum],
-              posicionesY: this.posicionesY[keyNum],
-              posicionesZ: this.posicionesZ[keyNum]
-            });
+        // const keys = Object.keys(this.posicionesX).map(Number).sort((a, b) => a - b);
+        if (this.direccion === "forward") {
+          for (const key in this.posicionesX) { //con hacerlo en X vale para las tres
+              const keyNum = Number(key);
 
-            this.keysUsadas.push(keyNum);
-          }
+              if (!(keyNum in this.keysUsadas) && ((keyNum - time) <= delta/1000)) { //dividido porque hablamos de milisegundos  
+                el.emit('mensaje', {
+                  posicionesX: this.posicionesX[keyNum],
+                  posicionesY: this.posicionesY[keyNum],
+                  posicionesZ: this.posicionesZ[keyNum]
+                });
+
+                this.keysUsadas.push(keyNum);
+              }// hacer lista con tiempos de cuando se pausa, retrocede, etc
+              else if (this.direccion === "backward") {
+                if (this.arranque) {
+                  this.keysEventos.push(keyNum);
+                  this.arranque = false;
+                }
+                tiempoActual = this.keysEventos[this.keysEventos.length - 1] - (time - this.keysEventos[this.keysEventos.length - 1]);
+
+                if (!(keyNum in this.keysUsadas) && ((keyNum - tiempoActual) <= delta/1000)) { //dividido porque hablamos de milisegundos
+
+                  el.emit('mensaje', {
+                      posicionesX: this.posicionesX[keyNum],
+                      posicionesY: this.posicionesY[keyNum],
+                      posicionesZ: this.posicionesZ[keyNum]
+                  });
+
+                  this.keysUsadas.pop();
+                }
+              }
+         }
         }
-      })
+      });
     }
 })
