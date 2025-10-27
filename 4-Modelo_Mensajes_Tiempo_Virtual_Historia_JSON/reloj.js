37f843935b7d87dfcf8825cc7047e40ee9225753
mcerdeno2021 <m.cerdeno.2021@alumnos.urjc.es>
2025-10-27 17:22:25 +0100

    Controles puestos como botón en el HTML (parece que funciona bien los controles anidados), las huellas no funcionan bien

--- 4-Modelo_Mensajes_Tiempo_Virtual_Historia_JSON/reloj.js
+++ 4-Modelo_Mensajes_Tiempo_Virtual_Historia_JSON/reloj.js
@@ -5,12 +5,20 @@ AFRAME.registerComponent('reloj', {
     this.pausado = false;
     this.direccion = 1; // 1 para avanzar, -1 para retroceder
 
-    this.el.addEventListener('control', e => {
-      const accion = e.detail.accion;
-      if (accion === 'pausar') this.pausado = true;
-      else if (accion === 'reanudar') this.pausado = false;
-      else if (accion === 'retroceder') this.direccion = -1;
-      else if (accion === 'avanzar') this.direccion = 1;
+    // BOTÓN PAUSA / REANUDAR
+    document.querySelector('#btn-pausa').addEventListener('click', () => {
+      this.pausado = !this.pausado; // alternar estado
+      console.log(this.pausado ? "⏸️ Pausado" : "▶️ Reanudado");
+    });
+
+    // BOTÓN AVANZAR
+    document.querySelector('#btn-avanzar').addEventListener('click', () => {
+      this.direccion = 1;
+    });
+
+    // BOTÓN RETROCEDER
+    document.querySelector('#btn-retroceder').addEventListener('click', () => {
+      this.direccion = -1;
     });
   },
 
@@ -18,8 +26,7 @@ AFRAME.registerComponent('reloj', {
     if (this.pausado) return; // detiene
 
     this.time += (delta / 1000) * this.direccion * this.velocidad;
-    console.log("Reloj tiempo:", this.time, delta/1000);
     
-    this.el.emit('reloj-tick', {time: this.time, delta: delta });
+    this.el.emit('reloj-tick', {time: this.time, delta: delta});
   }
 });
