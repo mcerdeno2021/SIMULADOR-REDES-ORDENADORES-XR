949fb2ae8e3419193cbac5897487eb5c69231e69
mcerdeno2021 <m.cerdeno.2021@alumnos.urjc.es>
2025-10-29 18:03:20 +0100

    Mensajes limpio

--- 4-Modelo_Mensajes_Tiempo_Virtual_Historia_JSON/mensaje.js
+++ 4-Modelo_Mensajes_Tiempo_Virtual_Historia_JSON/mensaje.js
@@ -4,42 +4,40 @@ AFRAME.registerComponent('mensaje', {
     this.huellas = {}; // diccionario para huellas
 
     el.addEventListener('mensaje', e => {
-      const { id, x, y, z, progreso } = e.detail;
+      const {id, x, y, z, progreso} = e.detail;
 
-      el.setAttribute('geometry', 'primitive: box');
+      el.setAttribute('geometry', 'primitive: box; width: 0.8; height: 0.8');
       el.setAttribute('material', 'color: #96ff2d');
       el.setAttribute('position', `${x} ${y} ${z}`);
 
-      // ✅ Inicializar array si no existe
-      if (!this.huellas[id]) this.huellas[id] = [];
+      if (!this.huellas[id]) this.huellas[id] = []; // Inicializar array si no existe
 
       const huella = document.createElement('a-entity');
+      const colorHuella = el.getAttribute('material').color;
       huella.setAttribute('position', `${x} ${y} ${z}`);
       huella.setAttribute('geometry', 'primitive: sphere; radius: 0.1');
-      const colorHuella = el.getAttribute('material').color;
       huella.setAttribute('material', `color: ${colorHuella}; opacity: 0.6`);
       huella.setAttribute('class', 'huella');
 
-      huella.dataset.progreso = progreso
+      huella.dataset.progreso = progreso // Guarda el valor de progreso como un atributo HTML llamado data-progreso dentro del elemento
 
       this.huellas[id].push(huella);
       el.sceneEl.appendChild(huella);
 
-      huella.addEventListener('click', evt => {
-        const pos = evt.target.getAttribute('position');
+      huella.addEventListener('click', e => {
+        const pos = e.target.getAttribute('position');
         console.log("Huella en", pos, "→ mensaje", id);
       });
     });
 
     el.addEventListener('borrar-huella', e => {
-      const { id, progresoActual } = e.detail;
+      const {id, progresoMensaje} = e.detail;
       if (!this.huellas[id]) return;
 
-      // Filtrar huellas a borrar: progreso > progresoActual
-      const huellasABorrar = this.huellas[id].filter(h => parseFloat(h.dataset.progreso) > progresoActual);
-      huellasABorrar.forEach(h => {
-        el.sceneEl.removeChild(h);
-        this.huellas[id] = this.huellas[id].filter(x => x !== h);
+      const huellasBorrar = this.huellas[id].filter(huella => huella.dataset.progreso > progresoMensaje); // Filtrar huellas a borrar: progresoHuella > progresoMensaje
+      huellasBorrar.forEach(huella => {
+        el.sceneEl.removeChild(huella);
+        this.huellas[id] = this.huellas[id].filter(x => x !== huella); // Coge las huellas que ya existen y mantén todas menos la que sea igual a huella
       });
     });
   }
